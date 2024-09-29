2475662ddc2420f8c0f48a1dfd4df18d36730db1
jprodrigogarcia0126 <jprodrigogarcia01.26@gmail.com>
2024-09-29 20:44:13 +0200

    Creación de colección de animales.

--- 04GestionDeAnimales/Program.cs
+++ 04GestionDeAnimales/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace _04GestionDeAnimales
     {
         public static void Main()
         {
-
+            ArrayList animales= new ArrayList();
         }
     }
 }
