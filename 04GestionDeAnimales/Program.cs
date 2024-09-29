999d8290beb838aa64cb933dd27ad977c3fb58be
jprodrigogarcia0126 <jprodrigogarcia01.26@gmail.com>
2024-09-29 20:51:23 +0200

    Creación de un menú de usuario.

--- 04GestionDeAnimales/Program.cs
+++ 04GestionDeAnimales/Program.cs
@@ -12,6 +12,18 @@ namespace _04GestionDeAnimales
         public static void Main()
         {
             ArrayList animales= new ArrayList();
+            int opcMenu;
+
+            do
+            {
+                Console.WriteLine("Selecciona una opción:" +
+                    "\n1. Añadir nuevo animal" +
+                    "\n2. Mostrar información de todos los animales" +
+                    "\n3. Buscar animal por nombre" +
+                    "\n4. Eliminar animal");
+
+                opcMenu = int.Parse(Console.ReadLine());
+            } while (opcMenu > 0 && opcMenu < 5);
         }
     }
 }
